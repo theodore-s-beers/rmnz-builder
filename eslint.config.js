23e1e2aca8dc33f3cdc402321a2fe35ebd7f9476
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-08-21 12:32:45 -0400

    Use new ESLint config format

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+
+export default [
+  {
+    ignores: ["**/*.min.js"],
+  },
+
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        document: "readonly",
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+  },
+];
